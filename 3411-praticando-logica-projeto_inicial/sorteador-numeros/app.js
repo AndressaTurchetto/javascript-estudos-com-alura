f1ee6ea39a51fd73228b4128d228d02890c4be0b
Bowie <145608228+AndressaTurchetto@users.noreply.github.com>
2024-03-01 15:08:54 -0300

    Atualização exercicio

---
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -0,0 +1,10 @@
+function sortear (){
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.documentElementById('de').value;
+    let ate = document.documentElementById('ate').value;
+
+    alert(`Quantidade: ${quantidade}`);
+    alert(`Do número: ${de}`);
+    alert(`Até o número: ${ate}`);
+    
+}
